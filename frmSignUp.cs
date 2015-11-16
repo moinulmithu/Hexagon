a5d23e73134b10b9f1177c75b778379e939a2241
Moinul Islam <moinulmithu@hotmail.com>
2015-11-17 01:13:03 +0600

    First User Create

--- frmSignUp.cs
+++ frmSignUp.cs
@@ -12,9 +12,23 @@ namespace Hexagon
 {
     public partial class frmSignUp : Form
     {
+        HexagonDBEntities db = new HexagonDBEntities();
+        User entity;
         public frmSignUp()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateUser_Click(object sender, EventArgs e)
+        {
+            User entity = new User();
+            entity.LogInName = txtLogInName.Text;
+            entity.LoginId = txtLogInId.Text;
+            entity.Password = txtPassword.Text;
+            db.Users.Add(entity);
+            db.SaveChanges();
+            MessageBox.Show("User created successfully");
+
         }
     }
 }
